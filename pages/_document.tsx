67192663bd73be1ea04260f16dc8938d4e4dd9d9
brunoksato <bruno.sato@live.com>
2021-07-19 00:39:12 -0300

    fix favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
         <Html lang='en'>
           <Head>
-            <link rel='shortcut icon' href='/favicon.png' />
+            <link rel='shortcut icon' href='/favicon.ico' />
 
             <link
               rel='apple-touch-icon'
